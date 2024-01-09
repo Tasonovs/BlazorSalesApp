9e0decc97a241779023ed9d8251e494f06e45204
s.shvyryov <s.shvyryov@itransition.com>
2024-01-09 15:24:15 +0300

    [feature] Add BlazorSalesAppContext

---
+++ BlazorSalesApp.Domain/Extensions/ChangeTrackerExtensions.cs
@@ -0,0 +1,44 @@
+﻿using BlazorSalesApp.Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BlazorSalesApp.Domain.Extensions;
+
+public static class ChangeTrackerExtensions
+{
+    public static void SetAuditProperties(this ChangeTracker changeTracker)
+    {
+        var entityEntries =
+            changeTracker.Entries()
+                .Where(entry => entry.Entity is BaseEntity &&
+                            entry.State is EntityState.Deleted or EntityState.Added or EntityState.Modified);
+
+        if (!entityEntries.Any())
+        {
+            return;
+        }
+
+        var timestamp = DateTime.UtcNow;
+
+        foreach (var entry in entityEntries)
+        {
+            var entity = (BaseEntity) entry.Entity;
+
+            if (entry.State == EntityState.Added)
+            {
+                entity.CreatedUtc = timestamp;
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                entity.UpdatedUtc = timestamp;
+                entry.Property(nameof(BaseEntity.CreatedUtc)).IsModified = false;
+            }
+            else if (entry.State == EntityState.Deleted)
+            {
+                entity.DeletedUtc = timestamp;
+                entry.Property(nameof(BaseEntity.CreatedUtc)).IsModified = false;
+                entry.Property(nameof(BaseEntity.UpdatedUtc)).IsModified = false;
+            }
+        }
+    }
+}
---
+++ BlazorSalesApp.Infrastructure/BlazorSalesAppContext.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+using BlazorSalesApp.Domain.Extensions;
+using BlazorSalesApp.Domain.Models.Common;
+using BlazorSalesApp.Domain.Models.Orders;
+using BlazorSalesApp.Domain.Models.SubElements;
+using BlazorSalesApp.Domain.Models.Windows;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSalesApp.Infrastructure;
+
+public class BlazorSalesAppContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Window> Windows { get; set; }
+    public DbSet<SubElement> SubElements { get; set; }
+
+    public override int SaveChanges()
+    {
+        return base.SaveChangesAsync(CancellationToken.None).GetAwaiter().GetResult();
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        ChangeTracker.DetectChanges();
+
+        ChangeTracker.SetAuditProperties();
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var defaultSchemaNameForDatabase = "dbo";
+        modelBuilder.HasDefaultSchema(defaultSchemaNameForDatabase);
+        base.OnModelCreating(modelBuilder);
+
+        ChangeEntitiesDeleteBehaviorToRestrict(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        SeedLookups(modelBuilder);
+    }
+
+    private static void ChangeEntitiesDeleteBehaviorToRestrict(ModelBuilder modelBuilder)
+    {
+        var cascadeForeignKeys = modelBuilder.Model.GetEntityTypes()
+            .Where(it =>
+                Type.GetType(it.Name)?.BaseType?.Name is nameof(BaseEntity) or nameof(BaseLookupEntity))
+            .SelectMany(t => t.GetForeignKeys())
+            .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+        foreach (var foreignKey in cascadeForeignKeys)
+        {
+            foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+
+    private static void SeedLookups(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OrderStateLookup>()
+            .HasData(new List<OrderStateLookup>
+            {
+                new()
+                {
+                    Id = 1,
+                    Label = "NY",
+                },
+                new()
+                {
+                    Id = 2,
+                    Label = "CA",
+                },
+            });
+
+        modelBuilder.Entity<SubElementTypeLookup>()
+            .HasData(new List<SubElementTypeLookup>
+            {
+                new()
+                {
+                    Id = 1,
+                    Label = "Window",
+                },
+                new()
+                {
+                    Id = 2,
+                    Label = "Doors",
+                },
+            });
+    }
+}
