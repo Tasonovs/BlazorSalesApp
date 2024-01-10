59143ac15d60be0fdaa5db097b9842619bec9569
s.shvyryov <s.shvyryov@itransition.com>
2024-01-11 01:34:34 +0300

    [feature] UI added

--- BlazorSalesApp.Ui/Program.cs
+++ BlazorSalesApp.Ui/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorSalesApp.Ui;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,6 @@ builder.Services.AddScoped(_ => new HttpClient
 {
     BaseAddress = new Uri(baseAddress)
 });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
